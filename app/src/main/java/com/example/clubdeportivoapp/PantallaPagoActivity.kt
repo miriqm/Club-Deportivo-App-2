13bc179db1b1617b0d6e43cb57791b053e601144
Miriam <120044756+miriqm@users.noreply.github.com>
2024-10-27 23:08:59 -0300

    PantallaPagoActivity: se cambio checkbox por radiobutton en forma de pago y se agrego validaciones

--- app/src/main/java/com/example/clubdeportivoapp/PantallaPagoActivity.kt
+++ app/src/main/java/com/example/clubdeportivoapp/PantallaPagoActivity.kt
@@ -3,6 +3,9 @@ package com.example.clubdeportivoapp
 import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
+import android.widget.EditText
+import android.widget.RadioButton
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import com.example.clubdeportivoapp.R.id.btnVolver
@@ -13,21 +16,39 @@ class PantallaPagoActivity : AppCompatActivity() {
         enableEdgeToEdge()
         setContentView(R.layout.activity_pantalla_pago)
 
+
+        val rbEfectivo = findViewById<RadioButton>(R.id.rbEfectivo)
+        val rbTarjeta = findViewById<RadioButton>(R.id.rbTarjeta)
+        val txtMonto = findViewById<EditText>(R.id.txtMonto)
+        val btnPagar =findViewById<Button>(R.id.btnPagar)
+        val btnComprobante = findViewById<Button>(R.id.btnComprobante)
+
+        val monto = txtMonto.text.toString()
         val btnVolver = findViewById<Button>(btnVolver)
 
+        btnPagar.setOnClickListener{
+            if(rbEfectivo.isChecked){
+                Toast.makeText(this,"Opcion: Efectivo", Toast.LENGTH_SHORT).show()
+            } else if(rbTarjeta.isChecked){
+                Toast.makeText(this,"Opcion: Tarjeta", Toast.LENGTH_SHORT).show()
+            } else {
+                Toast.makeText(this,"Selecciona por favor una opcion", Toast.LENGTH_SHORT).show()
+            }
 
-
-        btnVolver.setOnClickListener {
-            val intent = Intent(this, PantallaPrincipal::class.java)
-            startActivity(intent)
+            if(monto.isEmpty()){
+                Toast.makeText(this, "Por favor,ingrese el importe a pagar", Toast.LENGTH_SHORT).show()
+                return@setOnClickListener
+            }
         }
 
 
-
-        val btnComprobante = findViewById<Button>(R.id.btnComprobante)
-
         btnComprobante.setOnClickListener {
             val intent = Intent(this, PantallaComprobante::class.java)
+            startActivity(intent)
+        }
+
+        btnVolver.setOnClickListener {
+            val intent = Intent(this, PantallaPrincipal::class.java)
             startActivity(intent)
         }
 
